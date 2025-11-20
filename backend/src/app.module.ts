efd5698771e2f83c37580d3647da49e030191e91
Nelson Cadenas <nca1478@yahoo.com>
2025-11-19 23:17:32 -0400

    feat(backend): configure I18nModule in app module with language resolvers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './infrastructure/modules/auth.module';
-import { UserModule } from './infrastructure/modules/user.module';
-import { TaskModule } from './infrastructure/modules/task.module';
+import { Module } from "@nestjs/common";
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from "nestjs-i18n";
+import * as path from "path";
+import { AuthModule } from "./infrastructure/modules/auth.module";
+import { UserModule } from "./infrastructure/modules/user.module";
+import { TaskModule } from "./infrastructure/modules/task.module";
 
 @Module({
-  imports: [AuthModule, UserModule, TaskModule],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: "en",
+      loaderOptions: {
+        path: path.join(__dirname, "/i18n/"),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ["lang"] },
+        AcceptLanguageResolver,
+      ],
+    }),
+    AuthModule,
+    UserModule,
+    TaskModule,
+  ],
 })
 export class AppModule {}
