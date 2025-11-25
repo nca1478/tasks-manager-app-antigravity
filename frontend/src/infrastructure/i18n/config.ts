0e5022e60979beaea21875e4f0c11f4f00bfd0f7
Nelson Cadenas <nca1478@yahoo.com>
2025-11-24 22:36:36 -0400

    fix: i18n config (frontend)

--- frontend/src/infrastructure/i18n/config.ts
+++ frontend/src/infrastructure/i18n/config.ts
@@ -8,4 +8,5 @@ export const defaultLocale: Locale = "en";
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`./messages/${locale}.json`)).default,
   timeZone: "America/New_York", // Configura tu zona horaria preferida
+  locale: locale as string,
 }));
